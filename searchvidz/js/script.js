5675c6bae52e3e72fcb03b4cb6e771dd57100f33
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2016-10-19 12:43:06 -0500

    Create display and styling of content and buttons

--- searchvidz/js/script.js
+++ searchvidz/js/script.js
@@ -37,7 +37,7 @@ function search() {
 	$('#buttons').html('');
 
 	// Get form input
-	var q = $("#query").val();
+	q = $("#query").val();
 
 	// Run GET request on API
 	$.get(
@@ -51,15 +51,54 @@ function search() {
 		function(data) {
 			var nextPageToken = data.nextPageToken;
 			var prevPageToken = data.prevPageToken;
-
+			console.log(data);
 			$.each(data.items, function(i, item) {
 				var output = getOutput(item);
 				$('#results').append(output);
 			});
+
+			var buttons = getButtons(prevPageToken, nextPageToken);
+
+			$('#buttons').append(buttons);
 		}
 	);
 }
 
-function getOutput(){
+function getOutput(item) {
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
 
+	var output = '<li>' +
+				'<div class="list-left">' +
+				'<img src="' + thumb + '">' +
+				'</div>' + 
+				'<div class="list-right">' + 
+				'<h3>' + title + '</h3>' +
+				'<small>By <span class="cTitle"> ' + channelTitle + ' </span> on ' + videoDate + '</small>' +
+				'<p>' + description + '</p>' +
+				'</div>' + 
+				'</li>' + 
+				'<div class="clearfix"></div>' +
+				'';
+	return output;
+}
+
+function getButtons(prevPageToken, nextPageToken) {
+	if(!prevPageToken) {
+		var btnoutput = '<div class="button-container"' +
+				'<button id="next-button" class="paging-button" data-token="' + 
+					nextPageToken + '" data-query="' + q + ' onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container"' +
+				'<button id="prev-button" class="paging-button" data-token="' + 
+					nextPageToken + '" data-query="' + q + ' onclick="prevPage();">Previous Page</button></div>';
+				'<button id="next-button" class="paging-button" data-token="' + 
+					nextPageToken + '" data-query="' + q + ' onclick="nextPage();">Next Page</button></div>'
+	}
+
+	return btnoutput;
 }
