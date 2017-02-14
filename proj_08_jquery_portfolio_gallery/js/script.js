33416c4b0a59abe29473dcdefe54160bec4e25ca
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2017-02-14 11:56:44 -0600

    Create toggle fade in and out behavior for projects gallery

--- proj_08_jquery_portfolio_gallery/js/script.js
+++ proj_08_jquery_portfolio_gallery/js/script.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
 	$('nav a').on('click', function() {
-		// Clear the current class from all
-		$('nav li').removeClass('current');
+		// Clear the 'current' class
+		$('nav li.current').removeClass('current');
 		// Add it to just the one you want it on
 		$(this).parent().addClass('current');
 		// Set heading text
 		$('h1.heading').text($(this).text());
+
+		// Get and filter link text
+		var category = $(this).text().toLowerCase().replace(' ', '-');
+		// Remove hidden class if 'all-projects' is selected
+		console.log(category)
+		if(category == "all-projects") {
+			$('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+		} else {
+			// Loop over all gallery items
+			$('ul#gallery li').each(function() {
+				// Check if it has the current item has a class which matches the current cagegory variable value
+				if(!$(this).hasClass(category)) {
+					// If not hide it and add the 'hidden' class
+					$(this).hide().addClass('hidden');
+				} else {
+					// Othwerwise fade it in and remove the hidden class
+					$(this).fadeIn('slow').removeClass('hidden');
+				}
+			});
+		}
+		// Stop any default link behavior
+		return false;
 	});
 });
